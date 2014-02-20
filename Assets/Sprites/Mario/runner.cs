ea3e4a98f009070b6dc33ac029636b1a261557d9
Charles Cole <smashdaisaku@gmail.com>
2014-02-19 21:17:37 -0600

    Initial Commit

---
+++ Assets/Sprites/Mario/runner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class runner : MonoBehaviour {
+	
+	public float speed = 2.0f;
+	public float acceleration;
+	public static float distanceTraveled;
+	
+	void Update () {
+		transform.Translate(speed * Time.deltaTime, 0f, 0f);
+		distanceTraveled = transform.localPosition.x;
+	}
+}
---
+++ Assets/jump.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+	
+	public string jumpButton = "Fire1";
+	public float jumpPower = 10.0f;
+	public Animator anim;
+	public float minJumpDelay = 0.5f;
+	public Transform groundCheck;
+	private float jumpTime = 0.0f;
+	public bool grounded = false;
+	private bool jumped = false;
+	private bool canJump = true;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//cast a raycast to see if the player is touching anything on the "ground" layer.
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		jumpTime -= Time.deltaTime;
+		
+		if(jumpTime < 0)
+		{
+			
+			jumpTime = 0;
+			
+		}
+		if(Input.GetButtonDown(jumpButton) && canJump == true)
+		{
+			
+			canJump = false;
+			jumped = true;
+			grounded = false;
+			anim.SetTrigger("Jump");
+			rigidbody2D.AddForce(transform.up*jumpPower);
+			jumpTime = minJumpDelay;
+		}
+		
+		if(grounded && jumpTime <=0 && jumped)
+		{
+			canJump = true;
+			jumped = false;
+			anim.SetTrigger("Land");
+		}
+	}
+	
+}
