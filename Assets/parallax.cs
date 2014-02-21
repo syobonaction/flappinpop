ed65f9e7191276ee9bb801db07fc65d8c3dc00b7
pinkgrenades <jonathan.ferriter@gmail.com>
2014-02-21 01:17:30 -0600

    parallax background
    Added the backgrounds.

---
+++ Assets/parallax.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parallax : MonoBehaviour {
+
+	public float speed = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		renderer.material.mainTextureOffset = new Vector2(Time.time * speed,0);
+	
+	}
+}
